848d98febc8c946816e70e59ba4b8e85c8fd7377
Orm Finnendahl <orm.finnendahl@selma.hfmdk-frankfurt.de>
2015-03-27 14:47:37 +0100

    integrated ofo-utils.synth.sampled-piano

--- src/ofo_utils/api.clj
+++ src/ofo_utils/api.clj
@@ -2,10 +2,12 @@
   (:import [java.lang.management ManagementFactory])
   (:require clojure.stacktrace
             [ofo-utils.gui vumeter]
+            [ofo-utils.synth sampled-piano]
             )
   (:use [overtone.live]
         [overtone.helpers.ns]))
 
 (defn immigrate-ofo-utils-api []
   (immigrate 'overtone.live
-             'ofo-utils.gui.vumeter))
+             'ofo-utils.gui.vumeter
+             'ofo-utils.synth.sampled-piano))
